e73b123a424cbe32962a3304888c70e8eed23f39
Victor Feitosa <victorfeitosasouza@gmail.com>
2017-11-08 19:36:22 -0400

    Fix settings

--- ORF/settings.py
+++ ORF/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'sigos',
         'USER': 'root',
-        'PASSWORD': 'senha123',
+        'PASSWORD': 'toor',
         'TEST': {
             'CHARSET': 'utf8',
         },
